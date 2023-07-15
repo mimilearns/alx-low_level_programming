c75803af92acba987d2f678f3b40fd277230471c
mimilearns <miminartey5@gmail.com>
2023-07-15 06:38:49 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'Print single digit numbers of base 10 starting from 0'
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar('0' + 0);
+	putchar('1' + 0);
+	putchar('2' + 0);
+	putchar('3' + 0);
+	putchar('4' + 0);
+	putchar('5' + 0);
+	putchar('6' + 0);
+	putchar('7' + 0);
+	putchar('8' + 0);
+	putchar('9' + 0);
+	putchar('\n');
+	return (0);
+}
